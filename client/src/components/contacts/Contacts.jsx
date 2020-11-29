7d3ad8dbf07b51c3071e06ef8509d178cf26328c
AmjadWebDev <amjadwebdev@gmail.com>
2020-11-29 23:37:50 +0100

    added filter function

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -5,14 +5,13 @@ import ContactItem from "./ContactItem";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
-  return (
-    <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
-    </Fragment>
-  );
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+
+  return <Fragment>{filtered !== null ? filtered.map((contact) => <ContactItem key={contact.id} contact={contact} />) : contacts.map((contact) => <ContactItem key={contact.id} contact={contact} />)}</Fragment>;
 };
 
 export default Contacts;
