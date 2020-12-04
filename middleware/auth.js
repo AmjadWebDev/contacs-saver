dd6b1efb81ab1829ebac0513a509931b509cce72
AmjadWebDev <amjadwebdev@gmail.com>
2020-12-04 01:47:29 +0100

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,10 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  //Get token from header
+  // Get token from header
   const token = req.header("x-auth-token");
 
-  // check if not token
+  // Check if not token
   if (!token) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
@@ -16,6 +16,6 @@ module.exports = function (req, res, next) {
     req.user = decoded.user;
     next();
   } catch (err) {
-    return res.status(401).json({ msg: "Token is not valid" });
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
